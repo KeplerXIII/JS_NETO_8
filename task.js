def1b04b318c4ec1d682d36d75d5d3c3580fc8e3
a.pushkin <a.v.pushkin@gmail.com>
2023-05-30 10:56:25 +0300

    [~] cosmetic

--- task.js
+++ task.js
@@ -1,18 +1,18 @@
 const adsElements = document.querySelectorAll('.rotator__case')
 
 setInterval(() => {
-    const currentElement = document.querySelector('.rotator__case.rotator__case_active')
+    const currentElement = document.querySelector('.rotator__case.rotator__case_active');
 
     adsElements.forEach(element => {
-        element.classList.remove('rotator__case_active')
+        element.classList.remove('rotator__case_active');
     });
 
     let nextElement = currentElement.nextElementSibling
     if (nextElement === null) {
         nextElement = currentElement.parentNode.firstElementChild
-      }
+      };
     
     nextElement.style.color = nextElement.getAttribute('data-color');
     nextElement.classList.add('rotator__case_active');
 
-}, 100)
+}, 100);
