c0b39db1e5c86cbe3a02f93c632b116ef73f5b2f
a.pushkin <a.v.pushkin@gmail.com>
2023-05-30 10:54:40 +0300

    completed task_2

---
+++ task.js
@@ -0,0 +1,19 @@
+const adsElements = document.querySelectorAll('.rotator__case')
+let changeTime = 100
+
+setInterval(() => {
+    const currentElement = document.querySelector('.rotator__case.rotator__case_active')
+
+    adsElements.forEach(element => {
+        element.classList.remove('rotator__case_active')
+    });
+
+    let nextElement = currentElement.nextElementSibling
+    if (nextElement === null) {
+        nextElement = currentElement.parentNode.firstElementChild
+      }
+    
+    nextElement.style.color = nextElement.getAttribute('data-color');
+    nextElement.classList.add('rotator__case_active');
+
+}, 100)
